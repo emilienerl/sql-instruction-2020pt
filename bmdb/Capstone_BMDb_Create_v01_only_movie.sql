a36fbe6d646943e06d5bcd1eaf2cdef4c188aa77
Emilie Nerl <emilie.nerl1@gmail.com>
2020-08-17 21:42:34 -0400

    added movies to movie database

--- bmdb/Capstone_BMDb_Create_v01_only_movie.sql
+++ bmdb/Capstone_BMDb_Create_v01_only_movie.sql
@@ -19,4 +19,9 @@ Director 	varchar(255) 	not null
  	(1, 'Star Wars', 1977, 'PG', 'George Lucas'),
  	(2, 'Sixteen Candles', 1984, 'PG', 'John Hughes'),
   	(3, 'Fifth Element', 1997, 'PG-13', 'John Hughes'),
- 	(4, 'Better Off Dead', 1985, 'PG', 'Savage Steve Holland');
+ 	(4, 'Better Off Dead', 1985, 'PG', 'Savage Steve Holland'),
+	(5, 'Blazing Saddles', 1974, 'R', 'Mel Brooks'),
+    (6, 'Clueless', 1995, 'PG-13', 'Amy Heckerling'),
+    (7, 'Role Models', 2008, 'R', 'David Wain'),
+    (8, 'Weekend at Bernie''s', 1989, 'PG-13', 'Ted Kotcheff')
+    ;
